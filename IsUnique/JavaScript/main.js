123c8005d6de007628fe23c0ed942fc35866cda8
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-06-11 15:01:08 +0100

    add main js

---
+++ IsUnique/JavaScript/main.js
@@ -0,0 +1,18 @@
+function allUniqueChars(string) {
+  // O(n^2) approach, no additional data structures used
+  // for each character, check remaining characters for duplicates
+  for (var i = 0; i < string.length; i++) {
+    for (var j = i + 1; j < string.length; j++) {
+      if (string[i] === string[j]) {
+        return false; // if match, return false
+      }
+    }
+  }
+  return true; // if no match, return true
+}
+
+/* TESTS */
+console.log(allUniqueChars("abcd"), "true");
+console.log(allUniqueChars("abccd"), "false");
+console.log(allUniqueChars("bhjjb"), "false");
+console.log(allUniqueChars("mdjq"), "true");
