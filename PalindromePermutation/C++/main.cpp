006ed0cdb7fd7d493724a87974164450c6a1a1d9
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2020-06-13 15:36:14 +0100

    Added Palidrume Permutation bitwise

--- PalindromePermutation/C++/main.cpp
+++ PalindromePermutation/C++/main.cpp
@@ -7,7 +7,7 @@
  * A permutation is a rearrangement of letters The palindrome does not need to be  limited to just dictionary words.
  */
 /**
- *
+ *using hash table solution
  * @param str
  * @return bool
  */
@@ -32,10 +32,30 @@ bool  palindromePermutation(const std::string & str) {
     }
     return (count < 2);
 }
+
+bool palidromePermutaionBitWise(const std::string & str) {
+    if(str.empty()) return false;
+     unsigned int bitVector= 0;
+    for(unsigned char it : str){
+        if( it >= 'a' && it <= 'z') {
+            it = it - 'a';
+            unsigned int mask =  1U << it;
+            bitVector ^= mask;
+        }
+        if( it >= 'A' && it <= 'Z') {
+            it = it - 'A';
+            unsigned int mask = ( 1U << it);
+            bitVector ^= mask;
+        }
+    }
+    return (bitVector & (bitVector-1)) == 0;
+}
 int main() {
     std::string word = "Tact Coa";
     std::cout<<word<< " : "<< std::boolalpha<<palindromePermutation(word)<< std::endl;
-    word = "";
+    std::cout<<word<< " : "<< std::boolalpha<<palidromePermutaionBitWise(word)<< std::endl;
+    word = "Code";
     std::cout<<word<< " : "<< std::boolalpha<<palindromePermutation(word)<< std::endl;
+    std::cout<<word<< " : "<< std::boolalpha<<palidromePermutaionBitWise(word)<< std::endl;
     return 0;
 }
